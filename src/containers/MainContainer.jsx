962ad8f5c9c9eb5f5c532d743fc0decc93ea40e2
miculnapoleon <aalexandra.tanase@gmail.com>
2023-11-23 23:33:17 +0100

    added footer changes

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,14 +1,95 @@
 import { messages } from "@/utils/constants";
 import styles from "./maincontainer.module.css";
+import Image from "next/image";
 
+
+const images =[
+    {
+        path:"",
+        height:"",
+        width:""
+    },
+    {
+        path:"",
+        height:"",
+        width:""
+    },
+    {
+        path:"",
+        height:"",
+        width:""
+    },
+    {
+        path:"",
+        height:"",
+        width:""
+    },
+    {
+        path:"",
+        height:"",
+        width:""
+    },
+    {
+        path:"",
+        height:"",
+        width:""
+    },
+    {
+        path:"",
+        height:"",
+        width:""
+    },
+    {
+        path:"",
+        height:"",
+        width:""
+    },
+    {
+        path:"",
+        height:"",
+        width:""
+    },
+    {
+        path:"",
+        height:"",
+        width:""
+    },
+]
 const MainContainer = () => {
   return (
-    <div>
+    <div className={styles.mainContainer}>
       <div className={styles.title}>{messages.title}</div>
       <div class={styles.bodyContent}>
+        {/* {
+            images.map((el,i)=>{
+                return (
+                <div className={styles.cardImage}>
+
+                </div>
+                )
+            })
+        } */}
       </div>
       <div className={styles.footer}>
-        {messages.footerCopyright}
+        <Image
+          src="icons/copyright.svg"
+          alt="copyright"
+          fill
+          className={styles.image}
+        />
+        <div className={styles.socialMedia}>
+          <a href="https://www.instagram.com/spontaneous_catches/">
+            <Image
+              src="icons/instagram.svg"
+              alt="instagram"
+              width={20}
+              height={20}
+            />
+          </a>
+          <a href="https://500px.com/p/aaalexandra?view=photos">
+            <Image src="icons/500px.svg" alt="500px" width={20} height={20} />
+          </a>
+        </div>
       </div>
     </div>
   );
